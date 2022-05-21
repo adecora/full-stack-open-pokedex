efda892639c744372b8a5350ba2f76a1e51a6171
adecora <alejandrodecora@gmail.com>
2022-05-22 01:01:29 +0200

    Adding health router on the app.js file

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 const express = require('express')
 const app = express()
+const healthRouter = require('./controllers/health')
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-// Simple endpoint for doing an application health check
-app.get('/health', (req, res) => {
-  res.send('ok')
-})
-
-// Dummy endpoint to ensure the deployed version
-app.get('/version', (req, res) => {
-  res.send('1')  // change this string to ensure a new version deployed
-})
+// End points to ensure our application health
+app.use('/', healthRouter)
 
 app.listen(PORT, () => {
   console.log('server started on port 5000')
