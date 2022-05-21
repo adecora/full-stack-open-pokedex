e90a57642a65667ee3f9de420d0f4d9a555226e1
adecora <alejandrodecora@gmail.com>
2022-05-21 21:33:42 +0200

    Adding healthcheck to heroku step on pipeline.yml

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// Simple endpoint for doing an application health check
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+// Dummy endpoint to ensure the deployed version
+app.get('/version', (req, res) => {
+  res.send('1')  // change this string to ensure a new version deployed
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
